c1af4c4421de1a19f7180bcaaa3ee9905ea27c70
Iacuzzo Giovanni <iacuzzogiovanni@gmail.com>
2012-11-21 14:33:32 +0100

    formualire contact terminer sans extension

--- footer.php
+++ footer.php
@@ -1,15 +1,5 @@
 ﻿        <footer>
-            <section id="newsLetter">
-                <form action="#">
-                    <fieldset>
-                        <h2>abonnez-vous à ma newsletter</h2>
-                        <input id="email" type="email" placeholder="entre votre e-mail" />
-                        <input id="abonner" type="submit" value="s'abonner" />
-                    </fieldset>
-                </form>
-            </section>
-            <section id="socialInfo">
-                <h2>Je suis social</h2>
+            <div id="socialInfo">
                 <ul>
                     <li class="social">
                         <a href="http://lnkd.in/Z8HBPA" title="vister ma page linkedin">mon linkedin</a>
@@ -18,10 +8,13 @@
                         <a href="http://www.facebook.com/GioGio8" title="vister ma page facebook">mon facebook</a>
                     </li>
                 </ul>
-            </section>
-            <section id="copy">
+            </div>
+            <div id="newsLetter">
+                <?php dynamic_sidebar(); ?>
+            </div>
+            <div id="copy">
                 <p>2012-Designed by Iacuzzo Giovanni©</p>
-            </section>
+            </div>
         </footer>
         <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.8.2.js"></script>
         <script src="<?php bloginfo('template_directory'); ?>/js/plugins.js"></script>
