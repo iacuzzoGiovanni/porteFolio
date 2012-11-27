50a268ef68a316246ec9f5e524a38f9a3e3109f1
Iacuzzo Giovanni <iacuzzogiovanni@gmail.com>
2012-11-27 15:55:47 +0100

    formulaire contact entièrement terminé

--- js/script.js
+++ js/script.js
@@ -10,80 +10,65 @@
 
 	var smoothScroll = function(e){
 		var the_id = $(this).attr("href"); 
-	    $('html, body').animate({  
-	        scrollTop:$(the_id).offset().top  
-	    }, 'slow');  
-	    return false; 
+		$('html, body').animate({  
+			scrollTop:$(the_id).offset().top  
+		}, 'slow');  
+		e.preventDefault();
 	};
 
-	var checkContactForm = function(e){
-		e.preventDefault();
-		
-		var sNom = $('#nom').val();
-		var sEmail = $('#email').val();
-		var sMessage = $('#texte').val();
-
-    	var checkIsFieldIsEmpty = function(){
-			var bool = 0;
-			
-			if(sNom === ""){
-				$("#nom").after('<p class="errors">le champ nom ne peut pas être vide</p>');
-			}else{
-				bool = 1;
-			}
-
-			if(sEmail === ""){
-				$("#email").after('<p class="errors">le champ e-mail ne peut pas être vide</p>');
-			}else{
-				bool = 1;
-			}
-
-			if(sMessage === ""){
-				$("#sendContactMail").before('<p class="errors">le champ message ne peut pas être vide</p>');
-			}else{
-				bool = 1;
-			}
-
-			return bool;
+	var checkIsNotEmpty = function(id, name){
+		var name = name ? name : id,
+			$field = $("#"+id);
+		if( !$field.val().length ){
+			$field.after('<p class="errors">le champ '+name+' ne peut pas être vide</p>');
+			return false;
 		}
+		return true;
+	};
 
-		if(checkIsFieldIsEmpty()){
-			$.ajax(
-				{
-					url:"http://localhost:8888/portefolio/contact-form.php",
-					type:"post",
-					data:{
-						nom:sNom,
-						email:sEmail,
-						texte:sMessage,
-						estAjax:1
-					},
-					success:function(data){
-						console.log(data);
-						if($('#sendResponse')){
-							$('#sendResponse').remove();
-							$('#nom').val('');
-							$('#email').val('');
-							$('#texte').val('');
-							$('<span id="sendResponse">'+data+'</span>').appendTo('#contactForm');
-						}else{
-							$('<span id="sendResponse">'+data+'</span>').appendTo('#contactForm');
-							$("#email").after('<p class="errors">'+data+'</p>');
-						}
-						
-					}
-
-				}
-			)
-		}	
-
+	var checkIsMailOk = function(id){
+		var $field = $("#"+id);
+		var reg = new RegExp('s^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$', 'i');
+		
+		if(!reg.test($field.val())){
+			$field.after('<p class="errors">le champ '+id+" n'est pas valide</p>");
+			return false;
+		}
+		return true;
 	}
+
+	var checkContactForm = function(e){
+
+		// keep this before the checks to remove the previous error messages
+		$('#sendResponse, .errors').remove();
+
+		var $contactForm = $('#contactForm'),
+			isNameOk = checkIsNotEmpty('nom'),
+			isemailOk = checkIsNotEmpty('email') && checkIsMailOk('email'),
+			istextOk = checkIsNotEmpty('texte');
+
+		if( isNameOk && isemailOk && istextOk ){
+
+			$.ajax({
+				url:"http://iacuzzo-giovanni.com/contact-form.php",
+				type:"post",
+				data: $contactForm.serialize(),
+				success:function(data){
+					$contactForm.append('<span id="sendResponse">'+data+'</span>')[0].reset();
+				},
+				error:function(data){
+					$("#email").after('<p class="errors">'+data.responseText+'</p>');
+				}
+			});
+		}
+		e.preventDefault();
+	};
 
 	//loaded pages
 	$(function(){
 		$('a[href^="#"]').on("click", smoothScroll);
-		$('#sendContactMail').on("click", checkContactForm);
+		$('#contactForm').on("submit", checkContactForm);
 	});
 
 
-} )( jQuery );
+}( jQuery ));
